6f7737c8f9446554d75a868cc7d3665b0209849d
NY64 <me@ny64.kr>
2023-02-05 16:58:33 +0900

    🙈 gitignore 업데이트

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const asyncify = require("express-asyncify");
-const express = require("express");
-const router = asyncify(express.Router());
-
-router.get("/", async (req, res) => {
-	res.render("index");
-});
-
-module.exports = router;
