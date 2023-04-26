b0e657612754fb64e1044401cbdad1f27fb957cf
NY64 <me@ny64.kr>
2023-04-26 09:25:50 +0900

    ✅ traffic 추가

---
+++ routes/api/traffic.js
@@ -0,0 +1,36 @@
+const asyncify = require("express-asyncify");
+const express = require("express");
+const router = asyncify(express.Router());
+
+const fs = require("fs");
+
+async function getNetworkUsage(interfaceName) {
+	try {
+		const data = await fs.promises.readFile("/proc/net/dev", "utf8");
+		const lines = data.trim().split("\n");
+		const interfaceLine = lines.find(line => line.includes(interfaceName));
+		if (!interfaceLine) {
+			throw new Error(`Interface ${interfaceName} not found in /proc/net/dev`);
+		}
+		const stats = interfaceLine.split(": ")[1].split(/\s+/);
+		const receivedBytes = parseInt(stats[0]);
+		const transmittedBytes = parseInt(stats[8]);
+		return { receivedBytes, transmittedBytes };
+	} catch (err) {
+		console.error(err);
+		return { receivedBytes: 0, transmittedBytes: 0 };
+	}
+}
+
+router.get("/", async (req, res) => {
+	const start = new Date();
+	const { receivedBytes, transmittedBytes } = await getNetworkUsage("ens18");
+
+	return res.status(200).json({
+		code: 200,
+		time: `${new Date() - start}ms`,
+		data: { rx: receivedBytes, tx: transmittedBytes, timeFrom: new Date() },
+	});
+});
+
+module.exports = router;
